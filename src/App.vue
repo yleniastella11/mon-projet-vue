584f0b3f8f4a92695c16de639861503d4a791180
stella ylenia <stellaylenia@MacBook-Pro-de-stella.local>
2024-12-04 09:41:53 +0100

    fourth commit

--- src/App.vue
+++ src/App.vue
@@ -22,63 +22,54 @@ const logo = [
 </script>
 
 <style scoped>
-header, main, footer{
-  width: 700px;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
-}
-
-header {
-  color: black;
-  background-color: #1F72D4;
-  line-height: 1.5;
-  margin: auto;
-}
-
-header h1 {
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  color: black;
-  background-color: white;
-  font-size: larger;
-  font-family: 'Times New Roman', Times, serif;
-}
-
-header img {
-  display: flex;
-  justify-content: center;
-  width: 100px;
-  height: 100px;
-  margin: auto;
-  padding: 1rem;
-}
-
-main {
+header, main, footer {
+    width: 700px;
+    display: flex; /* Ajouté pour activer Flexbox */
     flex-direction: column;
     justify-content: center;
     text-align: center;
+    align-items: center;
+}
+
+header {
     color: black;
-    background-color: #C9A6EB;
-    text-align: center;
+    background-color: #1F72D4;
+    line-height: 1.5;
     margin: auto;
 }
 
+header h1 {
+    color: black;
+    background-color: white;
+    font-size: larger;
+    font-family: 'Times New Roman', Times, serif;
+}
+
+header img {
+    display: flex;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    margin: auto;
+    padding: 1rem;
+}
+
+main {
+  color: black;
+  background-color: #C9A6EB;
+  margin: auto; 
+}
 
 footer {
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-  background-color: #1F72D4;
+    align-items: center;
+    margin: auto;
+    background-color: #1F72D4;
 }
 
 .logo-media {
-  display: inline-flex;
-  justify-content: center;
-  width: 50px;
-  height: 50px;
+    display: inline-flex;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
 }
 </style>
